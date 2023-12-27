367e6ef974777e4c3a047b4027cf97829e02c296
91626 <91626@LAPTOP-U7GEUGF8>
2023-12-28 04:05:00 +0530

    ProductAPI

---
+++ API/Data/DbIntializer.cs
@@ -0,0 +1,33 @@
+﻿using API.Entities;
+
+namespace API.Data
+{
+    public static class DbIntializer
+    {
+        public static void Initialize(StoreContext context)
+        {
+            if (context.Products.Any())
+                return;
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    Id =1,
+                    Name = "Green Angular Board 3000",
+                    Description = "Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus.",
+                    Price = 15000,
+                    PictureUrl = "/images/products/sb-ang2.png",
+                    Brand = "Angular",
+                    Type = "Boards",
+                    QuantityInStock = 100
+                }
+               
+            };
+            foreach(var product in products)
+            {
+                context.Products.Add(product);
+            }
+            context.SaveChanges();
+        }
+    }
+}
